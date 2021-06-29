bf074cadef65ca1776f5e03687069771ebbb56ac
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-06-29 10:45:57 -0500

    add promises and separate logic

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // new line
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/main.js',
@@ -13,9 +14,10 @@ module.exports = {
     contentBase: './dist'     
   },
   plugins: [
+    new Dotenv(),
     new CleanWebpackPlugin(), 
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'Weather',
       template: './src/index.html',
       inject: 'body'
     })
